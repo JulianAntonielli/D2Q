f016edc8dbef9126990cc50a8ab137ad9b643ebe
JulianAntonielli <jantonielli01@gmail.com>
2016-07-04 18:22:25 -0300

    Questions can now be saved and shown to the user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+
+  root 'welcome#index'
+
+  resources :articles
+
+  # For details on the DSL available within 
+  # this file, see http://guides.rubyonrails.org/routing.html
 end
